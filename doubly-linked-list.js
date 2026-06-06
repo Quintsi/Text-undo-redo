146cdb16ebd8930ce3c012df9b9d776adbc6ddc1
Quintsi <Evan.N.Sharp@gmail.com>
2026-06-06 16:43:29 -0400

    first commit

---
+++ doubly-linked-list.js
@@ -0,0 +1,48 @@
+class Node {
+    constructor(val){
+        this.data = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    append(val){
+        const newNode = new Node(val);
+
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    unshift(val){
+        const newNode = new Node(val);
+
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+
+}
